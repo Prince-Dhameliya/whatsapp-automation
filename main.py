49a8d8a7a5d29a803ad75a8ff2b1fba75e8c3907
dhameliyaprince <72374367+dhameliyaprince@users.noreply.github.com>
2021-10-31 23:44:38 +0530

    updated using pyautogui

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import webbrowser
+import pyautogui
 import time
 
-# Replace below path with the absolute path
-# to chromedriver in your computer
-driver = webdriver.Chrome('new/pp/chromedriver')
+person_name ='Prince' #input('enter a name whome you wan to sent a messages : ')
+type_msg = 'efejefewf eeeeeeeeeeeeeeeeeeee efffffffffffffffffffffv                   d ddddddddddddddddddddee         feefefef'#input('enter msg whatever you want tom send : ')
+
+webbrowser.open('https://web.whatsapp.com/')
+time.sleep(10)
+#print(pyautogui.position())
+
+pyautogui.click(286,259)
+pyautogui.typewrite(person_name)
+
+time.sleep(5)
+pyautogui.click(291,427) #163 259
+
+time.sleep(3)
+pyautogui.click(862,969) 
+pyautogui.typewrite(type_msg)
 
 
-driver.get("https://web.whatsapp.com/")
-wait = WebDriverWait(driver, 600)
+time.sleep(2)
+pyautogui.click(1790,970)
 
-# Replace 'Friend's Name' with the name of your friend
-# or the name of a group
-target = '"Friend\'s Name"'
 
-# Replace the below string with your own message
-string = "Message sent using Python!!!"
 
-x_arg = '//span[contains(@title,' + target + ')]'
-group_title = wait.until(EC.presence_of_element_located((
-	By.XPATH, x_arg)))
-group_title.click()
-inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
-input_box = wait.until(EC.presence_of_element_located((
-	By.XPATH, inp_xpath)))
-for i in range(100):
-	input_box.send_keys(string + Keys.ENTER)
-	time.sleep(1)
